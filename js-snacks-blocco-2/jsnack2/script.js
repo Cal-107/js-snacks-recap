1297922a3fa8954fb3bad6953e960c5a2237469a
“Cal-107” <kaly___7@hotmail.it>
2021-10-28 20:07:24 +0200

    add fix

--- js-snacks-blocco-2/jsnack2/script.js
+++ js-snacks-blocco-2/jsnack2/script.js
@@ -1,10 +1,9 @@
 // JSnack 3
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
-const nameList = ['Walter', 'julie', 'Jax'];
-const surnameList = ['Holmes', 'Teller', 'White'];
+const nameList = ['Walter', 'Julie', 'Jax', 'Jessie', 'Sylvie', 'Max', 'Loki', 'Tony', 'Steve'];
+const surnameList = ['Holmes', 'Teller', 'White', 'Pinkman', 'Wallace', 'Odinson', 'Stark', 'Rogers'];
 
-for (let i = 0; i < nameList.length; i++) {
-    console.log(`${nameList[Math.floor(Math.random() * 3)]} ${surnameList[Math.floor(Math.random() * 3)]}`)
+for (let i = 0; i < 3; i++) {
+    console.log(`${nameList[Math.floor(Math.random() * nameList.length)]} ${surnameList[Math.floor(Math.random() * surnameList.length)]}`);
 }
-

1217de1bb030a5e279f3ae9d856bbea38d81b1a0
“Cal-107” <kaly___7@hotmail.it>
2021-10-29 09:24:46 +0200

    add fix bonus

--- js-snacks-blocco-2/jsnack4/script.js
+++ js-snacks-blocco-2/jsnack4/script.js
@@ -3,7 +3,9 @@
 
 const arrayList1 = [1, 4 , 6, 7, 3, 10, 8, 20, 2, 27];
 const arrayList2 = [1, 4 , 6, 7, 3];
+
 console.log(`Numero elementi negli array iniziale: Primo array ${arrayList1.length}, secondo array ${arrayList2.length}`)
+
 
 while (arrayList2.length !== arrayList1.length) {
     arrayList2.push(Math.floor((Math.random() * 100) + 1))
